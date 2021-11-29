7971a21473db3a74d90b9a1bf62e1ce0c42baec6
Andrey Penechko <mrsmith33@yandex.ru>
2021-11-29 14:56:05 +0200

    Implement `Image views` chapter

--- host/deps/mimalloc.d
+++ host/deps/mimalloc.d
@@ -4,6 +4,7 @@ extern(C):
 void* mi_malloc(size_t size);
 void* mi_calloc(size_t count, size_t size);
 void* mi_realloc(void* p, size_t newsize);
+void* mi_recalloc(void *p, size_t count, size_t size);
 void* mi_expand(void* p, size_t newsize);
 void mi_free(void* p);
 // Extended functionality
@@ -15,3 +16,5 @@ void* mi_reallocn(void* p, size_t count, size_t size);
 void* mi_reallocf(void* p, size_t newsize);
 size_t mi_usable_size(void* p);
 size_t mi_good_size(size_t size);
+// Zero initialized re-allocation
+void* mi_rezalloc(void* p, size_t newsize);
